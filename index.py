54ba8d0a6dcde16b6bd5394490b9b579b3f585b7
exportDefaultDev <k.bardyugov@yandex.ru>
2023-04-16 21:55:37 +0500

    first commit

--- index.py
+++ index.py
@@ -1,16 +1,2 @@
-import sys
-from  functools import lru_cache
-sys.setrecursionlimit(10000)
-@lru_cache(None)
-def f(x):
-    if x <= 4:
-        return 1;
-    if x > 4:
-        return f(x - 1) + f(x - 3) + g(x - 2);
-@lru_cache(None)
-def g(x):
-    if x <= 1500:
-        return g(x + 1) + g(x + 2) + 1;
-    if x > 1500:
-        return 5;
-print((f(1200) + g(100)) % 10000)
+arr = [int(x) for x in open("17_7682.txt")]
+print(arr)
