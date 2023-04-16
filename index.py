7768e0b2353d9b4a22d11640a184c3095613bd2d
exportDefaultDev <k.bardyugov@yandex.ru>
2023-04-16 18:09:58 +0500

    sc

--- index.py
+++ index.py
@@ -1,7 +1,13 @@
-# (1xy20 + 3y11) / (4y11 – t18);
-
-for x in range(0, 19):
-    for y in range(0, 10):
-        for t in range(7):
-            first = (y * 20 ** 0 + x * 20 ** 1 + 1 * 20 ** 2) + (y * 11 ** 0 + 3 * 11 ** 1);
-            second = ()
+arr = [];
+alf = "ГЕЭ023";
+for x1 in alf:
+    for x2 in alf:
+        for x3 in alf:
+            for x4 in alf:
+                for x5 in alf:
+                    for x6 in alf:
+                        for x7 in alf:
+                            arr += [x1 + x2 + x3 + x4 + x5 + x6 + x7];
+first = [x for x in range(len(arr)) if arr[x] == "ЕГЭ2023"];
+second = [x for x in range(len(arr)) if arr[x] == "2023ЕГЭ"]
+print(second[0] - first[0] - 1)
