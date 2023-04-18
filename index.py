4b3d9f6b672db827e0e889c72183b959acd225a6
exportDefaultDev <k.bardyugov@yandex.ru>
2023-04-18 15:25:56 +0500

    s

--- index.py
+++ index.py
@@ -1,2 +1,17 @@
-arr = [int(x) for x in open("17_7682.txt")]
-print(arr)
+print(12341111111 < 10 ** 10);
+def findDels(num):
+    arr = set();
+    for x in range(1, round(num ** 0.5) + 1):
+        if num % x == 0:
+            arr.add(num // x);
+            arr.add(x);
+    if len(arr) == 2: return 1;
+    return 0;
+arr = set()
+for x in range(1, 1111111):
+    summ = sum(int(z) for z in str(x));
+    vuraz = (summ + 2) ** 3
+    value = int(f'1234{x}');
+    if value <= 10 ** 10 and value % vuraz and findDels(summ):
+        arr.add(summ);
+print(sorted(arr))
