e269d50710669804daa84a5a1a0e12c887f3ac52
Jermuck <k.bardyugov@yandex.ru>
2023-06-12 10:32:48 +0500

    adding new 26

--- index.py
+++ index.py
@@ -1,19 +1,40 @@
+import math
 with open('26.txt') as f:
     n = int(f.readline());
     k = int(f.readline());
-    l = int(f.readline())
-    arr = list(map(int, f.readlines()));
-arr.sort(reverse=True);
-kameras = [];
-indexes = [];
-t = 0;
-while len(kameras) != k and len(indexes) != n:
-    s = [];
-    for x in range(n):
-        if x not in indexes and sum(s) + arr[x] <= l:
-            s.append(arr[x]);
-            indexes.append(x);
-    kameras.append(s);
-    if len(s) != 0:
-        t = len(kameras);
-print(t)
+    price = int(f.readline());
+    arr = [];
+    for _ in range(n):
+        x, y, m = map(int, f.readline().split());
+        arr.append((x, y, m));
+kamera_1 = [0] * k
+kamera_2 = [0] * k
+kamera_3 = [0] * k
+money = 0;
+count = 0;
+arr.sort()
+for client in arr:
+    tLevel = client[2];
+    time = math.ceil((client[1] - client[0]) / 60 / 60);
+    if tLevel == 1:
+        for j in range(k):
+            if kamera_1[j] <= client[0]:
+                kamera_1[j] = client[1] + 60;
+                count += 1;
+                money += time * price * 1;
+                break;
+    if tLevel == 2:
+        for j in range(k):
+            if kamera_2[j] <= client[0]:
+                kamera_2[j] = client[1] + 60;
+                count += 1;
+                money += time * price * 2;
+                break;
+    if tLevel == 3:
+        for j in range(k):
+            if kamera_3[j] <= client[0]:
+                kamera_3[j] = client[1] + 60;
+                count += 1;
+                money += time * price * 4
+                break;
+print(count, money)
